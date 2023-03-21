faec762a0970b91a8ec60143a1a14fb671e8deda
Codejava <qwerttyuiop0374239142@gmail.com>
2023-03-21 23:11:10 +0700

    BTH_21_03

---
+++ BaiThucHanhLab3/BaiThucHanhLab3/src/Nhanvien.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Nhanvien {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String hoten;
+        int namsinh;
+        int hesoluong ;
+        final int luongcoban=1490000;
+        System.out.print("nhap ho ten:");
+        hoten = scanner.nextLine();
+        System.out.print("nhap nam sinh:");
+        namsinh = scanner.nextInt();
+        System.out.print("nhap he so luong:");
+        hesoluong = scanner.nextInt();
+        int luong = hesoluong * luongcoban;
+        System.out.print("luong la:" +luong);
+    }
+}
+    
+
